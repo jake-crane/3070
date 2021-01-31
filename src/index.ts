9420515c775ce4fa2b53be9abb128fbb81a72fc2
Jake Crane <jake@jakecrane.com>
2021-01-30 23:00:10 -0600

    Added newegg check

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import cheerio from "cheerio";
 import open from "open";
 import { setInterval } from "timers";
 
-const openAvailableItems = async () => {
+const openAvailableBestBuyItems = async () => {
     const date = new Date();
-    console.log(`Checking items ${date.toLocaleTimeString("en-US")} ${date.toLocaleDateString("en-US")}`);
+    console.log(`Checking bestbuy items ${date.toLocaleTimeString("en-US")} ${date.toLocaleDateString("en-US")}`);
     try {
         const resp = await axios.get(
             "https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=category_facet%3DGPUs%20%2F%20Video%20Graphics%20Cards~abcat0507002&sc=Global&st=3070%20rtx&type=page&usc=All%20Categories"
@@ -14,15 +14,29 @@ const openAvailableItems = async () => {
         const instockItems = $(".sku-item")
             .toArray()
             .filter(node => $(node).find(".add-to-cart-button:not(.btn-disabled)").length);
-        instockItems.forEach((item) => {
-            const href = `https://www.bestbuy.com${$(item).find("a").attr("href")}`;
-            console.log(`opening ${href}`);
-            open(href);
-        });
+        instockItems.forEach((item) => open(`https://www.bestbuy.com${$(item).find("a").attr("href")}`));
     } catch (e) {
         console.error(e);
     }
 }
 
-openAvailableItems();
-setInterval(openAvailableItems, 120_000);
+const openAvailableNewEggItems = async () => {
+    const date = new Date();
+    console.log(`Checking newegg items ${date.toLocaleTimeString("en-US")} ${date.toLocaleDateString("en-US")}`);
+    try {
+        const resp = await axios.get("https://www.newegg.com/p/pl?d=rtx+3070&N=100006662&LeftPriceRange=0+720");
+        const $ = cheerio.load(resp.data);
+        const instockItems = $(".item-cell")
+            .toArray()
+            .filter(node => $(node).find(".btn-primary").length);
+        instockItems.forEach((item) => open(`${$(item).find("a").attr("href")}`));
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+openAvailableBestBuyItems();
+setInterval(openAvailableBestBuyItems, 120_000);
+
+openAvailableNewEggItems();
+setInterval(openAvailableNewEggItems, 120_000);
